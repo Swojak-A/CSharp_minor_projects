0d53bdaa6a09321f50db1f1b553ec9da25201959
Swojak-A <swojak.a@gmail.com>
2018-01-02 13:14:50 +0100

    initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HEllo World!");
+
+            Console.WriteLine("What is your name, User?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello, {name}. Today is {date:d}, {date:t}.");
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
